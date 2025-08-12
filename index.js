b8c580127cfef753787ded476abbc69372b3a526
sreekanthv21 <kithuin21@gmail.com>
2025-08-12 11:59:32 +0530

    hi

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ app.get('/getimg',async(req,res)=>{
         }
         const buffer = Buffer.concat(chunks);
     res.set({
-        'Content-Type': 'image/png',
+        'Content-Type': 'image/jpg',
         'Content-Length': buffer.length,
         'Content-Disposition': 'inline',
         'Access-Control-Allow-Origin': '*', 
